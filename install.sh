e2d81bee3d96e399825e0ede37b295d9cb657117
Khashayar Hajian <me@khashayar.me>
2023-04-22 23:53:02 +0200

    Add scripts

--- install.sh
+++ install.sh
@@ -6,19 +6,9 @@ echo ""
 echo "🤚  This script will setup .dotfiles for you."
 read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 
-
-# Install Homebrew
-command -v brew >/dev/null 2>&1 || \
-  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
-
-# Install Oh My Zsh
-if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
-  (echo '💰  Installing oh-my-zsh' && yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
-fi
-
 # Install chezmoi
 command -v chezmoi >/dev/null 2>&1 || \
-  (echo '👊  Installing chezmoi' && brew install chezmoi)
+  (echo '👊  Installing chezmoi' && sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply khashayar)
 
 if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
   echo "🚸  chezmoi already initialized"
